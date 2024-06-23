ddb069872639a777d742659f0fc29678ad3ebad7
Paul W <73733025+pwalkerdev@users.noreply.github.com>
2024-06-23 17:39:38 +0800

    finish vscode integrated debugger mvp. still heaps to go but it fucking works 🎉

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,106 @@
 import * as vscode from 'vscode';
 import { headless, instance, uuid, prototerminal } from './global';
+//import { TransientSourceCodeDebugAdapterFactory } from './transientSourceCodeDebugAdapter';
+import { MemFS } from './fileSystemProvider';
+import { Breakpoint, BreakpointEvent, Source, LoggingDebugSession } from '@vscode/debugadapter';
+import { DebugProtocol } from '@vscode/debugprotocol';
+
 
 export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(vscode.workspace.registerFileSystemProvider('prototypervfs', new MemFS(), { isCaseSensitive: true }));
+
     context.subscriptions.push(vscode.commands.registerCommand('extension.prototyper.evaluate', () => Evaluate()));
-	context.subscriptions.push(vscode.commands.registerCommand('extension.prototyper.evaluatecsharp', () => Evaluate("CSharp")));
-	context.subscriptions.push(vscode.commands.registerCommand('extension.prototyper.evaluatejs', () => Evaluate("JavaScript")));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.prototyper.evaluatecsharp', () => Evaluate("CSharp")));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.prototyper.evaluatejs', () => Evaluate("JavaScript")));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.prototyper.debugcsharp', () => DebugHeadless()));
+
+    // context.subscriptions.push(vscode.debug.onDidStartDebugSession(session => { 
+    //     let loggingSession = session as unknown as LoggingDebugSession;
+    //     if (!loggingSession) {
+    //         return;
+    //     }
+    //     let bp = new Breakpoint(true, 1, 1, new Source(session.name, 'Untitled-1.cs')) as DebugProtocol.Breakpoint;
+    //     loggingSession.sendEvent(new BreakpointEvent('new', bp));
+    // }));
+
+    //const transientSourceCodeDebuggerAdapterFactory = new TransientSourceCodeDebugAdapterFactory();
+    //const factoryRef = vscode.debug.registerDebugAdapterDescriptorFactory('prototyper-csharp', transientSourceCodeDebuggerAdapterFactory)
+    //context.subscriptions.push(factoryRef);
+
+    // vscode.languages.registerDocumentLinkProvider(
+    //     { language: "csharp" },
+    //     {
+    //         provideDocumentLinks(document, token) {
+    //             return [new vscode.DocumentLink(
+    //                 new vscode.Range(new vscode.Position(3, 4), new vscode.Position(3, 4)),
+    //                 vscode.Uri.from({ scheme: 'untitled', path: 'Untitled-1' })
+    //               )];
+    //         },
+    //     }
+    //   );
+
+    // vscode.debug.registerDebugAdapterTrackerFactory('coreclr', {
+    //     createDebugAdapterTracker(session: vscode.DebugSession) {
+    //         return {
+    //             onWillReceiveMessage: m => {
+    //                 if (m.command === 'source') {
+    //                     console.log(`> ${JSON.stringify(m, undefined, 2)}`);
+    //                 }
+    //             },
+    //             onDidSendMessage: m => {
+    //                 if (m.command === 'source') {
+    //                     console.log(`> ${JSON.stringify(m, undefined, 2)}`);
+    //                 }
+    //             },
+    //         };
+    //       }
+    //   }
+    // );
 }
 
 function Evaluate(languageOverride: string | undefined = undefined): void {
-	if (vscode.window.activeTextEditor) {
-		RunHeadless(languageOverride, vscode.window.activeTextEditor, vscode.window.terminals.find(t => t.name === prototerminal.name));
-	}
+    if (vscode.window.activeTextEditor) {
+        RunHeadless(languageOverride, vscode.window.activeTextEditor, vscode.window.terminals.find(t => t.name === prototerminal.name));
+    }
 }
 
 function RunHeadless(languageOverride: string | undefined, editor: vscode.TextEditor, terminal: vscode.Terminal | undefined): void {
-	const executionDelay = terminal ? 0 : instance.terminalInitialisationDelay, // HACK: This is pretty crappy but if we create a new terminal then we have to wait until it is ready. I couldn't find a 'proper' way to do this
-		  token = uuid.new();
-	
-	(terminal ??= vscode.window.createTerminal(prototerminal.options)).sendText(`cmd.exe /c "${headless.Location}" -l ${languageOverride ?? editor.document.languageId} -i stream -t "${token}"`);
+    const executionDelay = terminal ? 0 : instance.terminalInitialisationDelay, // HACK: This is pretty crappy but if we create a new terminal then we have to wait until it is ready. I couldn't find a 'proper' way to do this
+          token = uuid.new();
+    
+    (terminal ??= vscode.window.createTerminal(prototerminal.options)).sendText(`cmd.exe /c "${headless.location}" -l ${languageOverride ?? editor.document.languageId} -i stream -t "${token}"`);
 
-	setTimeout(() => {
-		terminal!.sendText(''); // the empty line here serves no purpose - i just think it looks prettier
-		for (var i = 0; i < editor.document.lineCount; i++) {
-			terminal!.sendText(editor.document.lineAt(i).text);
-		}
-		terminal!.sendText(token); // Resending the token indicates to the script interpreter that there is no more to send
-	}, executionDelay);
+    setTimeout(() => {
+        terminal!.sendText(''); // the empty line here serves no purpose - i just think it looks prettier
+        for (var i = 0; i < editor.document.lineCount; i++) {
+            terminal!.sendText(editor.document.lineAt(i).text);
+        }
+        terminal!.sendText(token); // Resending the token indicates to the script interpreter that there is no more to send
+    }, executionDelay);
 
-	terminal.show(true);
+    terminal.show(true);
+}
+
+function DebugHeadless(): void {
+    if (vscode.window.activeTextEditor) {
+        const token = uuid.new(), terminal = vscode.window.terminals.find(t => t.name === prototerminal.name) ?? vscode.window.createTerminal(prototerminal.options);
+
+        //let documentUri = vscode.Uri.parse(`prototypervfs:/${token}.cs`);
+        let documentUri = vscode.Uri.parse(`prototypervfs:/Untitled-1.cs`);
+        vscode.workspace.fs.writeFile(documentUri, Buffer.from(vscode.window.activeTextEditor!.document.getText()));
+        vscode.window.showTextDocument(documentUri, { preview: false }).then(doc => {
+            terminal.sendText(`cmd.exe /c "${headless.location}" -l CSharp -m Debug -i stream -t "${token}"`);
+
+            setTimeout(() => {
+                vscode.debug.startDebugging(undefined, headless.debugConfiguration, undefined);
+                //terminal!.sendText(''); // the empty line here serves no purpose - i just think it looks prettier
+                for (var i = 0; i < doc.document.lineCount; i++) {
+                    terminal!.sendText(doc.document.lineAt(i).text);
+                }
+                terminal!.sendText(token); // Resending the token indicates to the script interpreter that there is no more to send
+            }, 2000);
+        
+            terminal.show(true);
+        });
+    }
 }
--- src/global.ts
+++ src/global.ts
@@ -10,7 +10,26 @@ export class instance {
 export class headless {
     private static readonly _searchPath: string[] = instance.isDebug ? ['.modules', 'Headless', 'Headless', 'bin', 'Debug', 'net8.0'] : ['dist', 'bin'];
 
-    static readonly Location: string = vscode.Uri.file(path.join(instance.installationFolder, ...this._searchPath, 'Headless.exe')).fsPath;
+    static readonly location: string = vscode.Uri.file(path.join(instance.installationFolder, ...this._searchPath, 'Headless.exe')).fsPath;
+
+    static readonly debugConfiguration: vscode.DebugConfiguration = {
+        name: '.NET Core Attach',
+        request: 'attach',
+        type: 'coreclr',
+        requireExactSource: false,
+        justMyCode: true,
+        processName: "Headless.exe",
+        stopAtEntry: true,
+        // symbolOptions: {
+        //     searchMicrosoftSymbolServer: true, // TODO: Maybe parameterise this?
+        //     moduleFilter: {
+        //         mode: "loadOnlyIncluded",
+        //         includedModules: [ "*" ],
+        //         includeSymbolsNextToModules: true
+        //     }
+        // }
+        
+    };
 }
 
 export class uuid {
